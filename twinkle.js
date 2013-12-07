2ce2d08d01071544ae2334e0167c909e77479a8a
zky829 <pineapple@sea.under>
2013-12-07 17:54:39 +0000

    Fix applicable of draggable.

--- twinkle.js
+++ twinkle.js
@@ -3,6 +3,19 @@ $(document).ready(function() {
 		$('.modal').fadeOut('fast');
 	});
 	
+	//TODO put .heart and .circle into array
+	
+	$('.heart, .circle').mouseover(function() {
+		$(this).fadeTo('slow', 0.1);
+	});
+
+	$('.heart, .circle').mouseleave(function() {
+		$(this).fadeTo('fast', 1);
+	});
+	
+	$('.heart, .circle').draggable();
+	
+/*
 	$('.shapes').mouseover(function() {
 		$(this).fadeTo('slow', 0.1);
 	});
@@ -12,7 +25,7 @@ $(document).ready(function() {
 	});
 	
 	$('.shapes').draggable();
-	
+*/	
 	$('p').hide();
 	$('ul').hide();
 });
